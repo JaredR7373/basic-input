0bd86be6c102516224142703cac74bbc5c997374
JaredR7373 <nubismoaf@gmail.com>
2018-12-20 14:45:31 -0500

    added ! To message

--- C++/src/basicInput.cpp
+++ C++/src/basicInput.cpp
@@ -6,9 +6,9 @@ int main()
 	string name;
 
 	cout << "Enter your name: ";
-	//cin >> name;
 	getline(cin, name);
-	cout << "Hello, " << name << endl;
+
+	cout << "Hello, " << name << "!\n";
 
 	return 0;
 }
